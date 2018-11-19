7e7bc98baa0fa00b0da4758d5dc162f8e99e7eec
Brett Commandeur <bcmdr.co@gmail.com>
2018-11-19 11:59:23 -0700

    fix twitter link in footer

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <TaskList />
     </section>
     <section class="footer">
-      GoDo © 2018 <a href="twitter.com/bcmdr">Brett Commandeur</a> | <a href="https://github.com/bcmdr/godo">Version 1.1.1</a>
+      GoDo © 2018 <a href="https://twitter.com/bcmdr">Brett Commandeur</a> | <a href="https://github.com/bcmdr/godo">Version 1.1.1</a>
     </section>
   </div>
 </template>
